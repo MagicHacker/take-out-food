7128fd48ec4f04c36f328b2842454c9a0f95aeaf
Johnson <zxmagichacker@gmail.com>
2020-02-16 15:31:19 +0800

    feat: 删除冗余代码

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <div id="app"></div>
 </template>
 
 <style lang="less">
@@ -19,7 +13,6 @@
 
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
