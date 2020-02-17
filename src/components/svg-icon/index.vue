4f6f6d9f77af02134ad3955dfdb682c076f49c7e
Johnson <zxmagichacker@gmail.com>
2020-02-17 11:49:33 +0800

    feat: 创建svg-icon组件

---
+++ src/components/svg-icon/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg class="icon">
+    <use xlink:href="" />
+  </svg>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+<style lang="less" scoped>
+.icon {
+  width: 1.5em;
+  height: 1.5em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
