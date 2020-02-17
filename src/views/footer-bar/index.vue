5e55e14ec87778256e21101088d755c7ea7cdd66
Johnson <zxmagichacker@gmail.com>
2020-02-17 11:50:28 +0800

    feat: 构建footer，主页，订单，搜索等组件

---
+++ src/views/footer-bar/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>footer</div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class FooterBar extends Vue {}
+</script>
+<style lang="less" scoped></style>
---
+++ src/views/order/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>order</div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+<style lang="less" scoped></style>
