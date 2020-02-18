fea6d7f6af805e61e0a093e14da31fd8e46d569f
Johnson <zxmagichacker@gmail.com>
2020-02-18 18:51:11 +0800

    feat: 构建headerTop组件

---
+++ src/components/header-top/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header-top-wrap">
+    <slot name="left"></slot>
+    {{ headerTitle }}
+    <slot name="right"></slot>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+@Component
+export default class HeaderTop extends Vue {
+  @Prop() headerTitle!: string;
+}
+</script>
+<style lang="less" scoped>
+.header-top-wrap {
+  width: 100%;
+  height: 40px;
+  background-color: rgb(172, 240, 178);
+  text-align: center;
+  line-height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>home</div>
+  <div class="home-page-wrap">
+    <header-top headerTitle="顶部">
+      <template v-slot:left>left</template>
+      <template v-slot:right>right</template>
+    </header-top>
+  </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({});
+import { Component, Vue } from "vue-property-decorator";
+import HeaderTop from "../../components/header-top/index.vue";
+@Component({
+  components: {
+    HeaderTop
+  }
+})
+export default class Home extends Vue {}
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-page-wrap {
+  width: 100%;
+}
+</style>
