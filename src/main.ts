d84ce409c2a675dbc62a61356b0c0a1467529fbb
Johnson <zxmagichacker@gmail.com>
2020-02-23 18:26:38 +0800

    feat: 引入tab，TextInput等相关组件

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Tabbar, Swiper } from "@nutui/nutui";
+import { Tabbar, Swiper, Tab, TabPanel, TextInput, Button } from "@nutui/nutui";
 import "./utils/svg-icon.ts";
-[Tabbar, Swiper].forEach(item => {
+[Tabbar, Swiper, Tab, TabPanel, TextInput, Button].forEach(item => {
   item.install(Vue);
 });
 Vue.config.productionTip = false;
